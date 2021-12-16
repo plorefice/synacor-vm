9c6938d167629beee6a43137bd3975eda1a9a20f
Pietro Lorefice <pietro.lorefice@gmail.com>
2021-12-16 20:06:34 +0100

    cli: read filename from args

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,29 @@
+use std::{fs::File, io::Read, path::PathBuf};
+
+use anyhow::Result;
+use structopt::StructOpt;
 use synacor::VirtualMachine;
 
-fn main() {
+#[derive(StructOpt)]
+struct Cli {
+    /// Loads a save state and resumes execution
+    #[structopt(short, long, parse(from_os_str))]
+    load_state: Option<PathBuf>,
+
+    /// Binary file containing the program to run
+    #[structopt(parse(from_os_str))]
+    program: PathBuf,
+}
+
+fn main() -> Result<()> {
+    let args = Cli::from_args();
+
     let mut vm = VirtualMachine::new();
 
-    vm.run(include_bytes!("../inputs/challenge.bin"));
+    let mut program = Vec::new();
+    File::open(args.program)?.read_to_end(&mut program)?;
+
+    vm.run(&program);
+
+    Ok(())
 }
